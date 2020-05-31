883d1fcfee2ffa266180900cf8cc213262e4a02a
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2020-05-31 21:16:04 +0700

    beautify + router

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import '../App.css';
 let apiKey = process.env.REACT_APP_APIKEY;
 
 export default function MovieDetails({ match }) {
 	useEffect(() => {
 		getJustOneMovie();
-		console.log(match);
 	}, []);
 
 	const [movie, setMovie] = useState({});
@@ -14,7 +14,7 @@ export default function MovieDetails({ match }) {
 			let url = `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${apiKey}&language=en-US`;
 			let data = await fetch(url);
 			if (data.status !== 200) {
-				throw new Error("data is wrong");
+				throw new Error('data is wrong');
 			}
 			let result = await data.json();
 			setMovie(result);
@@ -23,6 +23,48 @@ export default function MovieDetails({ match }) {
 			alert(err.message);
 		}
 	};
+	if (movie.genres === undefined) {
+		return <div>loading</div>;
+	}
+	return (
+		// <React.Fragment>
+		<div>
+			<div className="single-movie-card mx-auto">
+				<div className="placeholder">
+					<div
+						className="card-content"
+						style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`, backgroundSize: 'cover' }}
+					>
+						<div className="movie-title">
+							{movie.title} <span className="badge badge-danger score">{movie.vote_average}</span>
+						</div>
+						<div className="genre-lang">
+							{movie.genres.map((item) => {
+								return <span className="badge badge-warning genre">{item.name}</span>;
+							})}
+							{movie.spoken_languages.map((item) => {
+								return <span className="badge badge-primary genre">{item.name}</span>;
+							})}
+							&nbsp;&nbsp;&nbsp;
+							{movie.runtime}min
+						</div>
+						<hr className="style-four"></hr>
+						<div className="row">
+							<div className="left-side col-md-4 col-12">
+								<div>
+									<img className="movie-pic" src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt={movie.title} />
+								</div>
+							</div>
+							<div className="right-side col-md-8">
+								<div className="tagline">{movie.tagline}</div>
 
-	return <div>werewrew</div>;
+								<div className="movie-description">{movie.overview}</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		// </React.Fragment>
+	);
 }

de24e383e0ec44258957562dc63702e76b943ef4
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2020-06-16 06:33:02 +0700

    fix id

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import ReactModal from "react-modal";
-import ClipLoader from "react-spinners/ClipLoader";
-import { css } from "@emotion/core";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
+import ClipLoader from 'react-spinners/ClipLoader';
+import { css } from '@emotion/core';
+import { Button } from 'react-bootstrap';
 // import '../MovieDetails.css';
 let apiKey = process.env.REACT_APP_APIKEY;
 const override = css`
@@ -11,11 +11,11 @@ const override = css`
 	border-color: red;
 `;
 
-export default function MovieDetails({ match }) {
+export default function MovieDetails(props) {
 	useEffect(() => {
-		console.log("match is:", match);
 		getJustOneMovie();
 	}, []);
+	const urlParams = props.match.params;
 	let [modalOpen, setModalOpen] = useState(false);
 	const [movie, setMovie] = useState({});
 	const [video, setVideo] = useState(null);
@@ -27,10 +27,10 @@ export default function MovieDetails({ match }) {
 	};
 	const getJustOneMovie = async () => {
 		try {
-			let url = `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=${apiKey}&language=en-US`;
+			let url = `https://api.themoviedb.org/3/movie/${urlParams.id}?api_key=${apiKey}&language=en-US`;
 			let data = await fetch(url);
 			if (data.status !== 200) {
-				throw new Error("data is wrong");
+				throw new Error('data is wrong');
 			}
 			let result = await data.json();
 			setMovie(result);
@@ -43,10 +43,10 @@ export default function MovieDetails({ match }) {
 
 	const getVideo = async () => {
 		try {
-			let url = `https://api.themoviedb.org/3/movie/${match.params.id}/videos?api_key=${apiKey}&language=en-US`;
+			let url = `https://api.themoviedb.org/3/movie/${urlParams.id}/videos?api_key=${apiKey}&language=en-US`;
 			let data = await fetch(url);
 			if (data.status !== 200) {
-				throw new Error("data is wrong");
+				throw new Error('data is wrong');
 			}
 			let result = await data.json();
 			setVideo(result);
@@ -60,7 +60,7 @@ export default function MovieDetails({ match }) {
 		return (
 			<div class="preload d-flex justify-content-center align-items-center">
 				<div className="sweet-loading">
-					<ClipLoader css={override} size={150} color={"#123abc"} loading="true" />
+					<ClipLoader css={override} size={150} color={'#123abc'} loading="true" />
 				</div>
 			</div>
 		);
@@ -76,18 +76,18 @@ export default function MovieDetails({ match }) {
 				isOpen={modalOpen}
 			>
 				<button className="closeB" onClick={() => closeModal()}>
-					{" "}
-					Close{" "}
+					{' '}
+					Close{' '}
 				</button>
 				<div className="w-100 h-100 mx-auto">
-					{video.results.length < 1 ? "" : <iframe class="mx-auto" width="90%" height="90%" src={`https://www.youtube.com/embed/${video.results[0].key}`}></iframe>}
+					{video.results.length < 1 ? '' : <iframe class="mx-auto" width="90%" height="90%" src={`https://www.youtube.com/embed/${video.results[0].key}`}></iframe>}
 				</div>
 			</ReactModal>
 			<div className="single-movie-card mx-auto">
 				<div className="placeholder">
 					<div
 						className="card-content"
-						style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`, backgroundSize: "cover" }}
+						style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`, backgroundSize: 'cover' }}
 					>
 						<div className="movie-title">
 							{movie.title} <span className="badge badge-danger score">{movie.vote_average}</span>
